936fa6772cb746985f1fb2764396dc05b3525dad
rajivp2001 <rpushparajah@outlook.com>
2023-05-04 21:48:11 +1000

    Update streamlit_app.py
    Added more items and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My parents healthy diner')
 
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('Breakfast favourites')
+streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avocado Toast')
