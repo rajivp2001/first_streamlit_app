db0a62c9aaf375091e3812b81998a15d0ca4dca7
rajivp2001 <rpushparajah@outlook.com>
2023-05-04 21:48:49 +1000

    Update streamlit_app.py
    more sections

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣 Omega 3 and Blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket smoothie')
 streamlit.text('🐔 Hard boiled free-range egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
